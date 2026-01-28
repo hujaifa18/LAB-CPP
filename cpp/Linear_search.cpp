536e77adb612a18f7351f62061c9276caab3b819
hujaifa18 <abidhasanhujaifa1215@gmail.com>
2026-01-28 15:43:06 +0600

    Add Radix Sort implementation to Linear_search.cpp

--- cpp/Linear_search.cpp
+++ cpp/Linear_search.cpp
@@ -1,38 +1,63 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
+int getmax(int A[],int n)
 {
-    int n;
-    cin >> n;
-    int tar, tar2, tar3;
-    cin >> tar >> tar2 >> tar3;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int i, max= INT_MIN;
+    for(i=0;i<n;i++)
     {
-        cin >> arr[i];
-    }
-    bool flag = false;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] = tar)
+        if(A[i]>max)
         {
-            flag = true;
-            break;
+            max=A[i];
         }
     }
-    int count = 0;
-    for (int i = 0; i < n; i++)
+    return max;
+
+}
+void countSort(int A[],int n,int pos)
+{
+    int output[n];
+    vector<int>cnt;
+    for(int i=0;i<=10;i++)
     {
-        if (arr[i] == tar2)
-            count++;
+        cnt.push_back(0);
     }
-    cout << boolalpha << flag << endl;
-    cout << count << endl;
-    for (int i = 0; i < n; i++)
+    int r = cnt.size();
+    for(int i=0;i<n;i++)
     {
-        if (arr[i] == tar3)
-            cout << i << " ";
+        cnt[(A[i]/pos)%10]++;
     }
-    cout << endl;
+    for(int i=1;i<=r;i++)
+    {
+        cnt[i] = cnt[i]+cnt[i-1];
+    }
+    for(int i = n-1;i>=0;i--)
+    {
+        output[--cnt[(A[i]/pos)%10]] = A[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        A[i]=output[i];
+    }
+
+}
+void RadixSort(int A[],int n)
+{
+    int m =  getmax(A,n);
+    for(int pos=1;(m/pos)>0;pos*=10)
+    {
+        countSort(A,n,pos);
+    }
+}
+int main()
+{
+    int n=6;
+    int A[] = {97,57,208,699,125,734};
+    RadixSort(A,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
+
